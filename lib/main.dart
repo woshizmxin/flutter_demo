e961bccc7619cd209d832c8027f80f7e5adf29d7
aimeituan <aimeituan@meituan.com>
2018-06-24 12:39:32 +0800

    修改主题色；
    Signed-off-by: aimeituan <aimeituan@meituan.com>

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Hi Jamal",
+      theme: new ThemeData(
+        primaryColor: Colors.orange
+      ),
       home: new RandomWords()
     );
   }
